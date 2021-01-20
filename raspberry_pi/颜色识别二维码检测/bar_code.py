05bb26fe6ebc2078a84a402df3b05a33eeb19435
weixinpeng <2538971062@qq.com>
2021-01-20 20:27:25 +0800

    fix indent bug to make it identical to the bar_code.py as it on Desktop

--- raspberry_pi/颜色识别二维码检测/bar_code.py
+++ raspberry_pi/颜色识别二维码检测/bar_code.py
@@ -3,7 +3,7 @@ import numpy as np
 import pyzbar.pyzbar as pyzbar
 
 def QR_detect():
-    camera = cv2.VideoCapture(2)
+    camera = cv2.VideoCapture(0)
     #count = 0
     while True:
         camera.read()
@@ -24,6 +24,7 @@ def QR_detect():
             print(barcodes)
             for barcode in barcodes:
                 (x,y,w,h) = barcode.rect
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
                 barcodeDate = barcode.data.decode("utf-8")
                 #print(barcode.data)
                 #print(barcodeDate)
@@ -32,9 +33,9 @@ def QR_detect():
                 cv2.putText(img,text,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
                 print("[INFO] Found {} barcode: {}".format(barcodeType,barcodeDate))
                 cv2.imshow('img_result',img)
-                key_num = cv2.waitKey(0)
-                if key_num == ord('q'):
-                    break
+            key_num = cv2.waitKey(0)
+            if key_num == ord('q'):
+                break
         cv2.destroyAllWindows()
         camera.release()
         cv2.destroyAllWindows()
