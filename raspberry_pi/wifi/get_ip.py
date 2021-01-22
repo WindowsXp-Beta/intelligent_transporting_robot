4400375d36af11a1d1f79c38d49cc372b241dee8
weixinpeng <2538971062@qq.com>
2021-01-22 11:21:19 +0800

    add a python script to get the IP of your host comuter

---
+++ raspberry_pi/wifi/get_ip.py
@@ -0,0 +1,9 @@
+import socket
+
+try: 
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+    s.connect(('8.8.8.8',80)) 
+    ip = s.getsockname()[0] 
+finally: 
+    s.close() 
+print(ip)  
